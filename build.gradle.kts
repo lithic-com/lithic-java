fd6cf6cdeb0a3bd70479910bf7da2bd8848ad1ce
Alex Rattray <rattray.alex@gmail.com>
2023-01-27 17:19:29 -0500

    fix: sonatype repository url

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,9 @@ allprojects {
 nexusPublishing {
     repositories {
         sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+
             username.set(System.getenv("SONATYPE_USERNAME"))
             password.set(System.getenv("SONATYPE_PASSWORD"))
         }
