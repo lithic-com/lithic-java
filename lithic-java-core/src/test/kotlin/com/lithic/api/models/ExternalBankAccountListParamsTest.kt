ae1d7233b00798319073f447fe05c272021940ac
meorphis <meorphis@gmail.com>
2024-05-30 15:03:11 -0400

    Revert "fix(examples): generate correctly typed example snippets for enums"
    This reverts commit f0b7da3f06550a5dbff25e0b67425766241367f2.

--- lithic-java-core/src/test/kotlin/com/lithic/api/models/ExternalBankAccountListParamsTest.kt
+++ lithic-java-core/src/test/kotlin/com/lithic/api/models/ExternalBankAccountListParamsTest.kt
@@ -15,11 +15,11 @@ class ExternalBankAccountListParamsTest {
             .accountTypes(listOf(ExternalBankAccountListParams.AccountType.CHECKING))
             .countries(listOf("string"))
             .endingBefore("string")
-            .ownerTypes(listOf(OwnerType.BUSINESS))
+            .ownerTypes(listOf(OwnerType.INDIVIDUAL))
             .pageSize(123L)
             .startingAfter("string")
-            .states(listOf(ExternalBankAccountListParams.AccountState.CLOSED))
-            .verificationStates(listOf(ExternalBankAccountListParams.VerificationState.ENABLED))
+            .states(listOf(ExternalBankAccountListParams.AccountState.ENABLED))
+            .verificationStates(listOf(ExternalBankAccountListParams.VerificationState.PENDING))
             .build()
     }
 
@@ -31,11 +31,11 @@ class ExternalBankAccountListParamsTest {
                 .accountTypes(listOf(ExternalBankAccountListParams.AccountType.CHECKING))
                 .countries(listOf("string"))
                 .endingBefore("string")
-                .ownerTypes(listOf(OwnerType.BUSINESS))
+                .ownerTypes(listOf(OwnerType.INDIVIDUAL))
                 .pageSize(123L)
                 .startingAfter("string")
-                .states(listOf(ExternalBankAccountListParams.AccountState.CLOSED))
-                .verificationStates(listOf(ExternalBankAccountListParams.VerificationState.ENABLED))
+                .states(listOf(ExternalBankAccountListParams.AccountState.ENABLED))
+                .verificationStates(listOf(ExternalBankAccountListParams.VerificationState.PENDING))
                 .build()
         val expected = mutableMapOf<String, List<String>>()
         expected.put("account_token", listOf("182bd5e5-6e1a-4fe4-a799-aa6d9a6ab26e"))
@@ -45,13 +45,16 @@ class ExternalBankAccountListParamsTest {
         )
         expected.put("countries", listOf("string"))
         expected.put("ending_before", listOf("string"))
-        expected.put("owner_types", listOf(OwnerType.BUSINESS.toString()))
+        expected.put("owner_types", listOf(OwnerType.INDIVIDUAL.toString()))
         expected.put("page_size", listOf("123"))
         expected.put("starting_after", listOf("string"))
-        expected.put("states", listOf(ExternalBankAccountListParams.AccountState.CLOSED.toString()))
+        expected.put(
+            "states",
+            listOf(ExternalBankAccountListParams.AccountState.ENABLED.toString())
+        )
         expected.put(
             "verification_states",
-            listOf(ExternalBankAccountListParams.VerificationState.ENABLED.toString())
+            listOf(ExternalBankAccountListParams.VerificationState.PENDING.toString())
         )
         assertThat(params.getQueryParams()).isEqualTo(expected)
     }
