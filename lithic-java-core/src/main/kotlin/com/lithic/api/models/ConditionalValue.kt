08fd3715052ba80f9df989fea7530c28ee55fbaf
Mirek Klimos <m.klimos@lithic.com>
2025-12-11 09:48:12 +0100

    fix: fix auth rule conditional value parsing

--- lithic-java-core/src/main/kotlin/com/lithic/api/models/ConditionalValue.kt
+++ lithic-java-core/src/main/kotlin/com/lithic/api/models/ConditionalValue.kt
@@ -219,30 +219,28 @@ private constructor(
 
             val bestMatches =
                 sequenceOf(
-                        if (node.isTextual) {
-                            tryDeserialize(node, jacksonTypeRef<String>())?.let {
-                                ConditionalValue(regex = it, _json = json)
-                            }
-                        } else {
-                            null
-                        },
                         if (node.isNumber) {
                             tryDeserialize(node, jacksonTypeRef<Long>())?.let {
                                 ConditionalValue(number = it, _json = json)
                             }
-                        } else {
-                            null
-                        },
+                        } else null,
                         if (node.isArray) {
                             tryDeserialize(node, jacksonTypeRef<List<String>>())?.let {
                                 ConditionalValue(listOfStrings = it, _json = json)
                             }
-                        } else {
-                            null
-                        },
-                        tryDeserialize(node, jacksonTypeRef<OffsetDateTime>())?.let {
-                            ConditionalValue(timestamp = it, _json = json)
-                        },
+                        } else null,
+                        // Try timestamp before generic string, since timestamps serialize as
+                        // strings
+                        if (node.isTextual) {
+                            tryDeserialize(node, jacksonTypeRef<OffsetDateTime>())?.let {
+                                ConditionalValue(timestamp = it, _json = json)
+                            }
+                        } else null,
+                        if (node.isTextual) {
+                            tryDeserialize(node, jacksonTypeRef<String>())?.let {
+                                ConditionalValue(regex = it, _json = json)
+                            }
+                        } else null,
                     )
                     .filterNotNull()
                     .allMaxBy { it.validity() }
