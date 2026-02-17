3befdc7bf6d94d49aa4bde09b5edd4cbb05518e3
Mirek Klimos <m.klimos@lithic.com>
2026-02-17 14:01:55 +0100

    Migrate to stadardwebhooks for webhook signature verification

--- lithic-java-core/src/main/kotlin/com/lithic/api/services/async/WebhookServiceAsync.kt
+++ lithic-java-core/src/main/kotlin/com/lithic/api/services/async/WebhookServiceAsync.kt
@@ -4,10 +4,8 @@ package com.lithic.api.services.async
 
 import com.lithic.api.core.ClientOptions
 import com.lithic.api.core.JsonValue
-import com.lithic.api.core.UnwrapWebhookParams
 import com.lithic.api.core.http.Headers
 import com.lithic.api.errors.LithicInvalidDataException
-import com.lithic.api.errors.LithicWebhookException
 import com.lithic.api.models.ParsedWebhookEvent
 import java.util.function.Consumer
 
@@ -42,14 +40,6 @@ interface WebhookServiceAsync {
      * @throws LithicInvalidDataException if the body could not be parsed.
      */
     fun parseUnsafe(body: String): ParsedWebhookEvent
-
-    /**
-     * Unwraps a webhook event from its JSON representation.
-     *
-     * @throws LithicInvalidDataException if the body could not be parsed.
-     * @throws LithicWebhookException if the webhook signature could not be verified
-     */
-    fun parsed(unwrapParams: UnwrapWebhookParams): ParsedWebhookEvent
 
     /**
      * A view of [WebhookServiceAsync] that provides access to raw HTTP responses for each method.
--- lithic-java-core/src/main/kotlin/com/lithic/api/services/blocking/WebhookServiceImpl.kt
+++ lithic-java-core/src/main/kotlin/com/lithic/api/services/blocking/WebhookServiceImpl.kt
@@ -6,23 +6,13 @@ import com.fasterxml.jackson.core.JsonProcessingException
 import com.fasterxml.jackson.module.kotlin.jacksonTypeRef
 import com.lithic.api.core.ClientOptions
 import com.lithic.api.core.JsonValue
-import com.lithic.api.core.UnwrapWebhookParams
-import com.lithic.api.core.checkRequired
-import com.lithic.api.core.getRequiredHeader
 import com.lithic.api.core.http.Headers
 import com.lithic.api.errors.LithicException
 import com.lithic.api.errors.LithicInvalidDataException
 import com.lithic.api.errors.LithicWebhookException
 import com.lithic.api.models.ParsedWebhookEvent
 import com.standardwebhooks.Webhook
-import com.standardwebhooks.exceptions.WebhookVerificationException
-import java.security.MessageDigest
-import java.time.Duration
-import java.time.Instant
-import java.util.Base64
 import java.util.function.Consumer
-import javax.crypto.Mac
-import javax.crypto.spec.SecretKeySpec
 import kotlin.jvm.optionals.getOrNull
 
 class WebhookServiceImpl internal constructor(private val clientOptions: ClientOptions) :
@@ -54,53 +44,15 @@ class WebhookServiceImpl internal constructor(private val clientOptions: ClientO
                     "The webhook secret must either be set using the env var, LITHIC_WEBHOOK_SECRET, on the client class, or passed to this method"
                 )
 
-        val whsecret =
-            try {
-                Base64.getDecoder().decode(webhookSecret.removePrefix("whsec_"))
-            } catch (e: RuntimeException) {
-                throw LithicException("Invalid webhook secret")
-            }
+        try {
+            val headersMap =
+                headers.names().associateWith { name -> headers.values(name) }
 
-        val msgId = headers.getRequiredHeader("webhook-id")
-        val msgSignature = headers.getRequiredHeader("webhook-signature")
-        val msgTimestamp = headers.getRequiredHeader("webhook-timestamp")
-
-        val timestamp =
-            try {
-                Instant.ofEpochSecond(msgTimestamp.toLong())
-            } catch (e: RuntimeException) {
-                throw LithicException("Invalid signature headers", e)
-            }
-        val now = Instant.now(clientOptions.clock)
-
-        if (timestamp.isBefore(now.minus(Duration.ofMinutes(5)))) {
-            throw LithicException("Webhook timestamp too old")
+            val webhook = Webhook(webhookSecret)
+            webhook.verify(payload, headersMap)
+        } catch (e: Exception) {
+            throw LithicWebhookException("Could not verify webhook event signature", e)
         }
-        if (timestamp.isAfter(now.plus(Duration.ofMinutes(5)))) {
-            throw LithicException("Webhook timestamp too new")
-        }
-
-        val mac = Mac.getInstance("HmacSHA256")
-        mac.init(SecretKeySpec(whsecret, "HmacSHA256"))
-        val expectedSignature =
-            mac.doFinal("$msgId.${timestamp.epochSecond}.$payload".toByteArray())
-
-        msgSignature.splitToSequence(" ").forEach {
-            val parts = it.split(",")
-            if (parts.size != 2) {
-                return@forEach
-            }
-
-            if (parts[0] != "v1") {
-                return@forEach
-            }
-
-            if (MessageDigest.isEqual(Base64.getDecoder().decode(parts[1]), expectedSignature)) {
-                return
-            }
-        }
-
-        throw LithicException("None of the given webhook signatures match the expected signature")
     }
 
     /**
@@ -128,28 +80,6 @@ class WebhookServiceImpl internal constructor(private val clientOptions: ClientO
         } catch (e: Exception) {
             throw LithicInvalidDataException("Error parsing body", e)
         }
-
-    override fun parsed(unwrapParams: UnwrapWebhookParams): ParsedWebhookEvent {
-        val headers = unwrapParams.headers().getOrNull()
-        if (headers != null) {
-            try {
-                val webhookSecret =
-                    checkRequired(
-                        "webhookSecret",
-                        unwrapParams.secret().getOrNull()
-                            ?: clientOptions.webhookSecret().getOrNull(),
-                    )
-                val headersMap =
-                    headers.names().associateWith { name -> headers.values(name) }.toMap()
-
-                val webhook = Webhook(webhookSecret)
-                webhook.verify(unwrapParams.body(), headersMap)
-            } catch (e: WebhookVerificationException) {
-                throw LithicWebhookException("Could not verify webhook event signature", e)
-            }
-        }
-        return parsed(unwrapParams.body())
-    }
 
     class WithRawResponseImpl internal constructor(private val clientOptions: ClientOptions) :
         WebhookService.WithRawResponse {
